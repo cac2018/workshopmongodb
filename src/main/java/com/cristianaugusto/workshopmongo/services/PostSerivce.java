9b6e60a7daa0ddc8f3a03a7729d58c1804d463e5
Cristian Augusto <cristianaugusto2013@gmail.com>
2019-11-02 17:56:35 -0300

    Obtendo um post por id

---
+++ src/main/java/com/cristianaugusto/workshopmongo/services/PostSerivce.java
@@ -0,0 +1,28 @@
+package com.cristianaugusto.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cristianaugusto.workshopmongo.domain.Post;
+import com.cristianaugusto.workshopmongo.repository.PostRepository;
+import com.cristianaugusto.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostSerivce {
+
+	@Autowired
+	private PostRepository repo;
+	
+	
+	public Post findById(String id) {
+		Optional<Post> post = repo.findById(id);
+		if (post.isPresent()) {
+			return post.get();	
+		}
+		throw new ObjectNotFoundException("Objeto não encontrado");
+	}
+	
+	
+}
