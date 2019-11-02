e4b167e802b70cbae21dec81745c2b6f4026fb12
Cristian Augusto <cristianaugusto2013@gmail.com>
2019-11-02 19:19:09 -0300

    Consulta com vários critérios

--- src/main/java/com/cristianaugusto/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/cristianaugusto/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,6 @@
 package com.cristianaugusto.workshopmongo.repository;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -15,4 +16,7 @@ import com.cristianaugusto.workshopmongo.domain.Post;
 
 	@Query("{ 'title': { $regex: ?0, $options: 'i'}}")
 	List<Post> searchTitle(String text);
+	
+	@Query("{ $and: [ {date: {$gte: ?1} }, {date: {$lte: ?2} }, { $or: [{ 'title': { $regex: ?0, $options: 'i'}},{ 'body': { $regex: ?0, $options: 'i'}},{ 'comments.text': { $regex: ?0, $options: 'i'}} ] } ] }")
+	List<Post> fullSearch(String text, Date minDate, Date maxDate);
 }
