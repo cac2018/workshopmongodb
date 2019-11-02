860945ff46b56085e4c61c623421ca4e12201e32
Cristian Augusto <cristianaugusto2013@gmail.com>
2019-11-02 18:43:03 -0300

    Consulta simples com query methods

--- src/main/java/com/cristianaugusto/workshopmongo/resources/PostResource.java
+++ src/main/java/com/cristianaugusto/workshopmongo/resources/PostResource.java
@@ -1,13 +1,17 @@
 package com.cristianaugusto.workshopmongo.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cristianaugusto.workshopmongo.domain.Post;
+import com.cristianaugusto.workshopmongo.resources.util.URL;
 import com.cristianaugusto.workshopmongo.services.PostSerivce;
 
 @RestController
@@ -24,6 +28,12 @@ public class PostResource {
 		return ResponseEntity.ok().body( obj );
 	}
 	
+	@RequestMapping(value = "/titlesearch",method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+		text = URL.decodeParam(text);
+		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body( list );
+	}
 	
 }
 
--- src/main/java/com/cristianaugusto/workshopmongo/services/PostSerivce.java
+++ src/main/java/com/cristianaugusto/workshopmongo/services/PostSerivce.java
@@ -1,5 +1,6 @@
 package com.cristianaugusto.workshopmongo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +23,11 @@ public class PostSerivce {
 			return post.get();	
 		}
 		throw new ObjectNotFoundException("Objeto não encontrado");
+	}
+	
+	
+	public List<Post> findByTitle(String text){
+		return repo.findByTitleContainingIgnoreCase(text);
 	}
 	
 	
